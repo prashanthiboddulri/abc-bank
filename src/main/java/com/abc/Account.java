6f0872d071f60d6a337dc8e87aa1f6edb462cf50
prashanthiboddulri <prashanthibodduluri@gmail.com>
2016-07-28 19:54:59 -0400

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -1,6 +1,8 @@
 package com.abc;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class Account {
@@ -44,12 +46,23 @@ public void withdraw(double amount) {
 //            case SUPER_SAVINGS:
 //                if (amount <= 4000)
 //                    return 20;
+                
             case MAXI_SAVINGS:
-                if (amount <= 1000)
+            	Calendar cal = Calendar.getInstance();
+            	cal.setTime(new Date());
+            	cal.add(Calendar.DATE, -10);
+            	Date dateBefore10Days = cal.getTime();
+            	for (Transaction t: transactions){
+			Date lastTransDate = t.getTransactionDate();
+			if(lastTransDate.getTime()  > dateBefore10Days.getTime()) //need to compare todays date with the past 10 days.
+					return amount*0.05;
+				else
+					return amount*0.001;}
+               /* if (amount <= 1000)
                     return amount * 0.02;
                 if (amount <= 2000)
                     return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
+                return 70 + (amount-2000) * 0.1;*/
             default:
                 return amount * 0.001;
         }
